4ad1fb818c0728c424c29e3cc4bd87848822d8e2
JBSL <bvkvgxvt@gmail.com>
2025-05-05 22:58:17 +0200

    Added new POST /snippet/create handler to create snippets (placeholder)

--- main.go
+++ main.go
@@ -11,36 +11,33 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
-// Add a snippetView handler function
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	// Extract the value of the id wildcard from the request using r.PathValue()
-	// and try to convert it to an integer using the strconv.Atoi() function. If
-	// it can't be converted to an integer, or the value is less than 1, we
-	// return a 404 page not found response.
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
 
-	// Use the fmt.Sprintf() function to interpolate teh id value with a
-	// message, then write it as the HTTP response
 	msg := fmt.Sprintf("Display a specific snippet with ID %d", id)
 	w.Write([]byte(msg))
 }
 
-// Add a snippetCreate handler function
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a form for creating a ne snippet..."))
 }
 
+// Add a snippetCreatePost handler function
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Save a new snippet..."))
+}
+
 func main() {
-	// Register the two new handler functions and corresponding route patters with
-	// the servemux, in exactly the same way we did before.
 	mux := http.NewServeMux()
-	mux.HandleFunc("/{$}", home)                      // restrict this route to exact matches on / only
-	mux.HandleFunc("/snippet/view/{id}", snippetView) // Add the {id} wildcard segment
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	// Create the new route, which is restricted to POST requests only
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
 	log.Print("starting server on :4000")
 
