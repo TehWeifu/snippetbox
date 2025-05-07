5f95494a450beef691ab5db29fbbd8e584825435
JBSL <bvkvgxvt@gmail.com>
2025-05-07 19:42:34 +0200

    Created helper functions for error handling on the http.Handlers Fixed the deliberate error added in the previous commit

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,8 +7,6 @@ import (
 	"strconv"
 )
 
-// Change the signature of the home handler so it is defined as method against
-// *application
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
@@ -20,22 +18,13 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		// Because the home handler is now a method against the application
-		// struct it can access its fields, including the structured logger, We'll
-		// use this create a log entry at Error level containing the error
-		// message, also including the request method and URI as attributes to
-		// assist with debugging
-		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, r, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		// And we also need to update teh code here to use the structured logger
-		// too.
-		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, r, err)
 	}
 }
 
