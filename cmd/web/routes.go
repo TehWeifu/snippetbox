c942b78eded1f099e6aa8bab22d06a9573aa0a36
JBSL <bvkvgxvt@gmail.com>
2025-05-13 16:40:09 +0200

    Starting authorization implementation. Created routes for signup, login and logout. Created placeholder handlers. Updated the navigation bar

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,24 +13,20 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	// Crete a new middleware chain containing the middleware specific to our
-	// dynamic application routes. For now, this chain will only contain the
-	// LoadAndSave session middleware but we'll add more to it later.
 	dynamic := alice.New(app.sessionManager.LoadAndSave)
 
-	// Update these routes to use the new dynamic middleware chain followed by
-	// the appropriate handler function. Note that because the alice TheFunc()
-	// method returns a http.Handler (rather than a HandlerFunc) we also
-	// need to switch to registering the route using the mux.Handle() method.
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
 	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
 	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
 
-	// Create a middleware chain containing out 'standard' middleware
-	// which will be used for every request our application receives.
-	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+	// Add the five new routes, all of which use our 'dynamic' middleware chain.
+	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
+	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignupPost))
+	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogin))
+	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLoginPost))
+	mux.Handle("POST /user/logout", dynamic.ThenFunc(app.userLogout))
 
-	// Return the 'standard' middleware chain followed by the servemux.
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
 	return standard.Then(mux)
 }
