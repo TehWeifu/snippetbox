3eb1f06007047a0d55a36e0014aef76fab97e12d
JBSL <bvkvgxvt@gmail.com>
2025-05-15 21:07:27 +0200

    Created helper functions for http tests. Added new ping route

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,15 +11,10 @@ import (
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
-	// Use the http.FileServerFS() function to create a HTTP handler which
-	// serves the embedded files in ui.Files. It's important to note that our
-	// static files are contained in the "static" folder of the ui.Files
-	// embedded filesystem. So, for example, or CSS stylesheet is located at
-	// "static/css/main.css". This means that we no longer need to strip the
-	// prefix from the request URL -- any requests that start with /static/ can
-	// just be passed directly to the file server and the corresponding static
-	// file will be served (so long as it exists).
 	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
+
+	// Add a new GET /ping route
+	mux.HandleFunc("GET /ping", ping)
 
 	// Add the authenticate() middleware to the chain.
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
